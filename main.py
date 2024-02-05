caa54cb863731b1501727d36e5478cf3707056a8
Yunsung Lim <kwngjin@gmail.com>
2024-02-05 11:05:19 +0900

    jinja2 templates 설정

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
 
 from app.routes.board import board_router
 from app.routes.member import member_router
@@ -8,9 +11,13 @@ app = FastAPI()
 app.include_router(member_router)
 app.include_router(board_router, prefix='/board')
 
-@app.get("/")
-async def index():
-    return {"message": "Hello World"}
+# jinja2 설정
+templates = Jinja2Templates(directory='views/templates')
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(req: Request):
+    return templates.TemplateResponse('index.html', {'request': req})
 
 
 # 파이썬 코드로
