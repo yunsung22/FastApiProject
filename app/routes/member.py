1ad1a1d8401380c6fc0e6efc879a55fa4f8ce4be
Yunsung Lim <kwngjin@gmail.com>
2024-02-05 12:08:02 +0900

    jinja2 templates 설정2

--- app/routes/member.py
+++ app/routes/member.py
@@ -1,17 +1,24 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 member_router = APIRouter()
 
-@member_router.get('/join')
-def join():
-    return {'msg': 'Hello, Join!'}
+# jinja2 설정
+templates = Jinja2Templates(directory='views/templates')
+member_router.mount('/static', StaticFiles(directory='views/static'), name='static')
+
+@member_router.get('/join', response_class=HTMLResponse)
+def join(req: Request):
+    return templates.TemplateResponse('join.html', {'request': req})
 
 
-@member_router.get('/login')
-def login():
-    return {'msg': 'Hello, Login!'}
+@member_router.get('/login', response_class=HTMLResponse)
+def login(req: Request):
+    return templates.TemplateResponse('login.html', {'request': req})
 
 
-@member_router.get('/myinfo')
-def myinfo():
-    return {'msg': 'Hello, myinfo!'}
+@member_router.get('/myinfo', response_class=HTMLResponse)
+def myinfo(req: Request):
+    return templates.TemplateResponse('myinfo.html', {'request': req})
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-
+from fastapi.staticfiles import StaticFiles
 
 from app.routes.board import board_router
 from app.routes.member import member_router
@@ -13,9 +13,10 @@ app.include_router(board_router, prefix='/board')
 
 # jinja2 설정
 templates = Jinja2Templates(directory='views/templates')
+app.mount('/static', StaticFiles(directory='views/static'), name='static')
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/index", response_class=HTMLResponse)
 async def index(req: Request):
     return templates.TemplateResponse('index.html', {'request': req})
 
